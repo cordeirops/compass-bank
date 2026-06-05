f3b402288cceba83f5f9cbad029b047fe0c2fc7a
Pedro S Cordeiro <cordeiro.hitss@celepar.pr.gov.br>
2026-06-05 13:10:56 -0300

    Primeiro commit

---
+++ src/test/java/org/cordeirops/compassbank/CompassBankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cordeirops.compassbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompassBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
