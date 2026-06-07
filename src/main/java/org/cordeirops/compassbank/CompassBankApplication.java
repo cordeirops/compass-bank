cf6dd5f9046326fb349b632c8f3452bc9820f2b2
cordeirops <pedro.sbarainicordeiro@gmail.com>
2026-06-07 16:19:19 -0300

    chore: configurar projeto e dependências Maven

---
+++ src/main/java/org/cordeirops/compassbank/CompassBankApplication.java
@@ -0,0 +1,13 @@
+package org.cordeirops.compassbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompassBankApplication.class, args);
+    }
+
+}
